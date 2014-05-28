2b802407a67d2d3c14c4167e3e52d12b9ba49037
Guillermo Rauch <rauchg@gmail.com>
2014-05-28 09:31:39 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
