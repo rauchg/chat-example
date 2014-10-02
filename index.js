e1910d2832f1096f2ba10c97ca406c7367cd1a6e
Davis Abubakr-Sadik Nii Nai <dwa2pac@gmail.com>
2014-10-02 19:15:49 +0000

    fix sendFile deprecated.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile('index.html');
 });
 
 io.on('connection', function(socket){
