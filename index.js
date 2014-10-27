37f8a1e9ef8a4500eb9489249f80f747fd54701e
Guillermo Rauch <rauchg@gmail.com>
2014-10-26 21:09:43 -0700

    make runnable from anywhere

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
