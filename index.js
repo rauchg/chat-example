a6221a8dee6e6eecbd7cf9293bdfb4ad5add3a30
Guillermo Rauch <rauchg@gmail.com>
2014-11-21 02:46:19 +0100

    use `sendFile`

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
