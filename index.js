174aa14d9a3a22221bafc53241b3764701b739a3
Jordan Sutton <jorsutton92@gmail.com>
2016-06-03 08:18:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on http.address().port');
 });
