3d74a3c716a337b3bce29d4c6b8fe867f45cc27f
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2021-01-06 11:46:04 +0100

    refactor: use ES6 syntax for the server code
    We'll keep ES5 syntax for the client code so it still runs in IE11.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
