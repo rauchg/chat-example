0ee7376e038b3b1a78d1ff25c7d7e737b2a8c0ca
Olakunle Raheem <olaks.raheem@gmail.com>
2023-01-25 22:39:50 +0200

    refactor: properly compute the path of the index.html file (#80)

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
+const path = require('path')
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
